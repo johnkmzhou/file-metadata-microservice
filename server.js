5bfc47b2d2bfbad6e93b6fa5f40736664253e9e7
John Zhou <john.km.zhou@gmail.com>
2018-08-30 10:19:33 -1000

    Add endpoint to retrieve file metadata.

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
-
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
      res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.post('/api/fileanalyse', multer().single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.send({name: originalname, type: mimetype, size});
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Node.js listening ...');
 });
